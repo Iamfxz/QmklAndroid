2be3e4475b42378ea77f8240b45d343699a30373
dddlovelll <38395934+dddlovelll@users.noreply.github.com>
2018-07-19 16:42:55 +0800

    正在写广告界面

--- app/src/main/java/com/android/papers/qmkl_android/activity/SplashActivity.java
+++ app/src/main/java/com/android/papers/qmkl_android/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package com.android.papers.qmkl_android.activity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -10,8 +11,17 @@ import android.util.Log;
 import android.view.WindowManager;
 
 import com.android.papers.qmkl_android.R;
+import com.android.papers.qmkl_android.model.AdData;
+import com.android.papers.qmkl_android.model.ResponseInfo;
 import com.android.papers.qmkl_android.util.PermissionUtils;
 import com.android.papers.qmkl_android.util.RetrofitUtils;
+import com.android.papers.qmkl_android.util.SDCardUtils;
+
+import java.io.File;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SplashActivity extends Activity {
 
@@ -33,12 +43,47 @@ public class SplashActivity extends Activity {
 //        RetrofitUtils.postLogin(this,request);
 
 
-        RetrofitUtils.postAd(this);
+        Call<ResponseInfo<AdData>> call=RetrofitUtils.postAd(this);
+        //发送网络请求(异步)
+        call.enqueue(new Callback<ResponseInfo<AdData>>() {
+            //请求成功时回调
+            @Override
+            public void onResponse(Call<ResponseInfo<AdData>> call, Response<ResponseInfo<AdData>> response) {
+                //广告页当前不可用
+                if(response.body().getData().isEnabled()){
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    nextActivity(LoginActivity.class);   //不显示广告,跳入mainactivity
+                                }
+                            });
+                        }
+                    }).start();
+                    Log.d(TAG, "已缓存");
+                    return;
+                }
+                //广告页当前可用
 
+
+            }
+            //请求失败时回调
+            @Override
+            public void onFailure(Call<ResponseInfo<AdData>> call, Throwable t) {
+                Log.d("123失败","失败");
+            }
+        });
 
         nextActivity(LoginActivity.class);
     }
-    private void nextActivity(Class clazz) {
+    public void nextActivity(Class clazz) {
         final Intent intent = new Intent(SplashActivity.this, clazz);
         new Thread(new Runnable() {
             @Override
@@ -60,6 +105,22 @@ public class SplashActivity extends Activity {
 
             }
         }).start();
+    }
+
+    /**
+     *
+     * 获取远程信息失败或者广告版本为最新时, 检查本地广告图片是否存在,若存在则显示广告页,不存在跳至主页
+     *
+     */
+    private void checkLocalADImage() {
+
+        Log.d(TAG, "检测本地广告图像是否存在");
+
+        File adImageFile = new File(SDCardUtils.getADImage());
+        Class clazz = adImageFile.exists() ? AdsActivity.class : LoginActivity.class;    //本地图片不存在, 跳转至mainactivity
+        nextActivity(clazz);
+        if(adImageFile.exists())Log.d(TAG, "本地广告图像存在");
+        else Log.d(TAG, "本地广告图像不存在");
     }
 
     public void onResume() {
--- app/src/main/java/com/android/papers/qmkl_android/util/RetrofitUtils.java
+++ app/src/main/java/com/android/papers/qmkl_android/util/RetrofitUtils.java
@@ -1,10 +1,12 @@
 package com.android.papers.qmkl_android.util;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
 import com.android.papers.qmkl_android.R;
+import com.android.papers.qmkl_android.activity.LoginActivity;
 import com.android.papers.qmkl_android.impl.PostAds;
 import com.android.papers.qmkl_android.impl.PostLogin;
 import com.android.papers.qmkl_android.model.AdData;
@@ -52,7 +54,7 @@ public class RetrofitUtils {
     }
 
     //获取广告
-    public static void postAd(Context context){
+    public static Call postAd(final Context context){
         //创建Retrofit对象
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(context.getString(R.string.base_url))// 设置 网络请求 Url,0.0.4版本
@@ -65,18 +67,7 @@ public class RetrofitUtils {
         //对 发送请求 进行封装(广告地址)
         Call<ResponseInfo<AdData>> call = request.getCall();
 
-        //发送网络请求(异步)
-        call.enqueue(new Callback<ResponseInfo<AdData>>() {
-            //请求成功时回调
-            @Override
-            public void onResponse(Call<ResponseInfo<AdData>> call, Response<ResponseInfo<AdData>> response) {
-                Log.d("123成功",response.body().getData().getFallback());
-            }
-            //请求失败时回调
-            @Override
-            public void onFailure(Call<ResponseInfo<AdData>> call, Throwable t) {
-                Log.d("123失败","失败");
-            }
-        });
+        return call;
+
     }
 }
